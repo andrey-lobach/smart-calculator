94fbc39f98103990b9f04c9989c3c87a896703c7
Andrei Lobach <andreylobach1997@gmail.com>
2018-09-29 18:08:33 +0300

    final commit

--- src/index.js
+++ src/index.js
@@ -1,27 +1,67 @@
-class SmartCalculator {
-  constructor(initialValue) {
-    // your implementation
-  }
+  class SmartCalculator {
+    constructor(initialValue) {
+      this.value = initialValue;
+      this.operations = [];
+      this.operations.push(this.value);
+    }
 
-  add(number) {
-    // your implementation
-  }
-  
-  subtract(number) {
-    // your implementation
-  }
+    add(number) {
+      this.operations.push("+");
+      this.operations.push(number);
+      return this;
+    }
 
-  multiply(number) {
-    // your implementation
-  }
+    subtract(number) {
+      this.operations.push("-");
+      this.operations.push(number);
+      return this;
+    }
 
-  devide(number) {
-    // your implementation
-  }
+    multiply(number) {
+      this.operations.push("*");
+      this.operations.push(number);
+      return this;
+    }
 
-  pow(number) {
-    // your implementation
+    devide(number) {
+      this.operations.push("/");
+      this.operations.push(number);
+      return this;
+    }
+
+    pow(number) {
+      this.operations.push("^");
+      this.operations.push(number);
+      return this;
+    }
+    toString() {
+        for (let i = this.operations.length - 1; i > 0; i--)
+            if (this.operations[i] == "^") {
+              this.operations.splice(i - 1, 3, Math.pow(this.operations[i - 1], this.operations[i + 1]));
+              i++;
+            }
+        for (let i = 0; i < this.operations.length; i++) {
+            if (this.operations[i] == "*") {
+              this.operations.splice(i - 1, 3, this.operations[i - 1] * this.operations[i + 1]);
+              i--;
+            }
+            if (this.operations[i] == "/") {
+              this.operations.splice(i - 1, 3, this.operations[i - 1] / this.operations[i + 1]);
+              i--;
+            }
+        }
+        for (let i = 0; i < this.operations.length; i++) {
+            if (this.operations[i] == "+") {
+              this.operations.splice(i - 1, 3, this.operations[i - 1] + this.operations[i + 1]);
+              i--;
+            }
+            if (this.operations[i] == "-") {
+              this.operations.splice(i - 1, 3, this.operations[i - 1] - this.operations[i + 1]);
+              i--;
+            }
+        }
+        return this.operations[0];
+    }
   }
-}
 
 module.exports = SmartCalculator;
